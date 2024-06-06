dbb00d6003b4983144ed46d91af1a78dcbca2ee9
kenstott <128912107+kenstott@users.noreply.github.com>
2024-06-05 21:25:43 -0700

    initial commit

---
+++ src/grpc/execute-graph-ql-query.ts
@@ -0,0 +1,53 @@
+import type { FormattedExecutionResult, GraphQLFormattedError } from 'graphql/index'
+import { type JsonObject, struct } from 'pb-util'
+import type { ObjMap } from 'graphql/jsutils/ObjMap'
+import { type Express } from 'express'
+
+export interface ExecuteGraphQLQuery {
+  operationName: string
+  query: string
+  variables: Record<string, unknown>
+  headers: Record<string, unknown>
+  callback: (_: null, response: { data?: any, errors?: any, extensions?: any }) => void
+}
+export function executeGraphQLQuery (app: Express) {
+  return ({ operationName, query, variables, headers, callback }: ExecuteGraphQLQuery): void => {
+    const req = {
+      method: 'POST',
+      url: '/graphql-internal',
+      params: {},
+      query: {},
+      headers: { 'Content-Type': 'application/json', ...headers }, // Request headers
+      body: { operationName, query, variables }
+    }
+    const res = {
+      setHeader: function (name: string, value: string) {
+        this.header = { ...this.header, [name]: value }
+      },
+      status: function (code: number) {
+        this.statusCode = code
+        return this // For method chaining
+      },
+      send: function (data: string) {
+        const parseData = JSON.parse(data) as FormattedExecutionResult
+        if (parseData.extensions) {
+          parseData.extensions = struct.encode(parseData.extensions as JsonObject) as ObjMap<unknown>
+        }
+        if (parseData.errors) {
+          parseData.errors = parseData.errors.map(i => struct.encode(i as unknown as JsonObject) as GraphQLFormattedError)
+        }
+        callback(null, parseData)
+      }
+    }
+    const next = function (err: Error): void {
+      if (err) {
+        // should be reflected in 'extensions'
+        res.status(500)
+      } else {
+        console.log('Route handled successfully')
+      }
+    }
+
+    app._router.handle(req, res, next)
+  }
+}
--- src/grpc/start-grpc-server.ts
+++ src/grpc/start-grpc-server.ts
@@ -1,105 +1,44 @@
-import grpc, { type UntypedHandleCall } from '@grpc/grpc-js'
+import grpc from '@grpc/grpc-js'
 import protoLoader from '@grpc/proto-loader'
 import { ReflectionService } from '@grpc/reflection'
 import * as fs from 'fs'
 import { type Express } from 'express'
-import { type JsonObject, struct } from 'pb-util'
-import type { FormattedExecutionResult, GraphQLFormattedError } from 'graphql'
-import { type ObjMap } from 'graphql/jsutils/ObjMap'
+import { type Struct, struct } from 'pb-util'
+import { executeGraphQLQuery } from './execute-graph-ql-query'
+import { type FormattedExecutionResult } from 'graphql/execution'
 
-interface MyProtoBuf extends grpc.ProtobufTypeDefinition {
-  GraphQLService: {
-    service: grpc.ServiceDefinition
-  }
+interface ServiceCall {
+  metadata: {
+    getMap: () => Record<string, unknown> }
+  request: { operationName: any, query: any, variablesStruct: any, variablesString: any }
 }
 
-interface GraphQLProtoImplementation extends grpc.UntypedServiceImplementation {
-  ExecuteQuery: UntypedHandleCall
-}
+type Callback = (_: null, response: FormattedExecutionResult) => void
 
 export const startServer = async (app: Express): Promise<void> => {
-  const PROTO_PATH = './proto/graphql.proto' // Path to your proto file
-  if (!fs.existsSync(PROTO_PATH)) {
+  if (!fs.existsSync(process.env.PROTO_PATH || '')) {
     return
   }
-  const packageDefinition = protoLoader.loadSync(PROTO_PATH)
-  const graphqlProto = grpc.loadPackageDefinition(packageDefinition).graphql as MyProtoBuf
+  const packageDefinition = protoLoader.loadSync(process.env.PROTO_PATH || '')
+  const graphqlProto = grpc.loadPackageDefinition(packageDefinition).graphql as grpc.GrpcObject
+  const graphQLService = (graphqlProto.GraphQLService as grpc.ServiceClientConstructor).service
   const server = new grpc.Server()
-
-  const executeGraphQLQuery = (
-    operationName: string,
-    query: string,
-    variables: Record<string, unknown>,
-    headers: Record<string, unknown>,
-    callback: (_: null, response: { data?: any, errors?: any, extensions?: any }) => void
-  ): void => {
-    const req = {
-      method: 'POST', // HTTP method (e.g., GET, POST, PUT, DELETE)
-      url: '/graphql-internal', // URL path
-      params: { }, // Route parameters
-      query: { }, // Query parameters
-      headers: { 'Content-Type': 'application/json', ...headers }, // Request headers
-      body: { operationName, query, variables }
-    }
-    const res = {
-      setHeader: function (name: string, value: string) {
-        this.header = { ...this.header, [name]: value }
-      },
-      status: function (code: string) {
-        this.statusCode = code
-        return this // For method chaining
-      },
-      send: function (data: string) {
-        const parseData = JSON.parse(data) as FormattedExecutionResult
-        if (parseData.extensions) {
-          parseData.extensions = struct.encode(parseData.extensions as JsonObject) as ObjMap<unknown>
-        }
-        if (parseData.errors) {
-          parseData.errors = parseData.errors.map(i => struct.encode(i as unknown as JsonObject) as GraphQLFormattedError)
-        }
-        callback(null, parseData)
-      }
-    }
-    const next = function (err: Error): void {
-      if (err) {
-        console.error('Error handling route:', err)
-        // Handle the error (e.g., send an error response)
-      } else {
-        // Continue to the next middleware or route handler
-        console.log('Route handled successfully')
-      }
-    }
-
-    app._router.handle(req, res, next)
-  }
-
   const reflection = new ReflectionService(packageDefinition)
   reflection.addToServer(server)
-  server.addService(graphqlProto.GraphQLService.service, {
-    ExecuteQuery: (call, callback) => {
+  server.addService(graphQLService, {
+    ExecuteQuery: (call: ServiceCall, callback: Callback) => {
       const headers = call.metadata.getMap()
-      let { operationName, query, variables, variablesString } = call.request
-      variables = variables || {}
-      if (variablesString) {
-        variables = JSON.parse(variablesString)
-      } else {
-        variables = struct.decode(variables)
-      }
-      executeGraphQLQuery(
-        operationName as string,
-        query as string,
-        variables as Record<string, string>,
-        headers as Record<string, string>,
-        callback)
+      const { operationName, query, variablesStruct, variablesString } = call.request
+      const variables = variablesString ? JSON.parse(variablesString as string) : struct.decode((variablesStruct ?? {}) as Struct)
+      executeGraphQLQuery(app)({ operationName, query, variables, headers, callback })
     }
-  } satisfies GraphQLProtoImplementation)
+  })
 
   server.bindAsync(`${process.env.SERVER_NAME || 'localhost'}:50051`, grpc.ServerCredentials.createInsecure(), (error, port) => {
     if (error) {
       console.error('Failed to bind server:', error)
-      return
+    } else {
+      console.log('GRPC Server bound on port:', port)
     }
-    console.log('Server bound on port:', port)
-    server.start()
   })
 }
